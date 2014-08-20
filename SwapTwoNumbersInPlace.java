cb8309423a0b619cf1764dbb32aad7089869bd22
Angela Pan <yp519@nyu.edu>
2014-08-20 19:58:31 -0400

    Create SwapTwoNumbersInPlace.java

---
+++ SwapTwoNumbersInPlace.java
@@ -0,0 +1,13 @@
+public class SwapTwoNumbersInPlace {
+    private static void swap(int num1, int num2){
+        
+    }
+    public static void main(String[] args){
+        int num1 = 4;
+        int num2 = 6;
+        System.out.println("Before swap num1: "+ num1 + " num2: "+ num2);
+        swap(num1, num2);
+        System.out.println("After swap num1: "+ num1 + " num2: "+ num2);
+        
+    }
+}
