84e4f7752401a8b07f94d3f49fb859234e696eee
Angela Pan <yp519@nyu.edu>
2014-08-21 12:06:43 -0400

    Update SwapTwoNumbersInPlace.java

--- SwapTwoNumbersInPlace.java
+++ SwapTwoNumbersInPlace.java
@@ -1,6 +1,8 @@
 public class SwapTwoNumbersInPlace {
     private static void swap(int num1, int num2){
-        
+        num1 = num1 - num2;
+        num2 = num1 + num2;
+        num1 = num2 - num1;
     }
     public static void main(String[] args){
         int num1 = 4;
