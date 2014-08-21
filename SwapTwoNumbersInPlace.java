0cdf6cf9100047c4cb74c958202ec34c46ab86f3
Angela Pan <yp519@nyu.edu>
2014-08-21 12:07:09 -0400

    Update SwapTwoNumbersInPlace.java

--- SwapTwoNumbersInPlace.java
+++ SwapTwoNumbersInPlace.java
@@ -3,13 +3,13 @@ public class SwapTwoNumbersInPlace {
         num1 = num1 - num2;
         num2 = num1 + num2;
         num1 = num2 - num1;
+        System.out.println("After swap num1: "+ num1 + " num2: "+ num2);
     }
     public static void main(String[] args){
         int num1 = 4;
         int num2 = 6;
         System.out.println("Before swap num1: "+ num1 + " num2: "+ num2);
         swap(num1, num2);
-        System.out.println("After swap num1: "+ num1 + " num2: "+ num2);
         
     }
 }
